5128212cb8139616174b9078266ff9108e00f0cc
Shinwoo Kim <shinwookim@pitt.edu>
2023-07-05 22:06:24 -0400

    Refactor to allow SQL Compiler

--- src/DBSQL.c
+++ src/DBSQL.c
@@ -12,6 +12,29 @@ typedef struct
     ssize_t input_length;
 } InputBuffer;
 
+typedef enum
+{
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum
+{
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+typedef enum
+{
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
+
+typedef struct
+{
+    StatementType type;
+} Statement;
+
 InputBuffer *new_input_buffer()
 {
     InputBuffer *input_buffer = (InputBuffer *)calloc(1, sizeof(InputBuffer));
@@ -30,19 +53,57 @@ void print_prompt()
 void read_input(InputBuffer *input_buffer)
 {
     ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
-    if (bytes_read <=0) 
+    if (bytes_read <= 0)
     {
         printf("Error reading input\n");
         exit(EXIT_FAILURE);
     }
 
-    input_buffer->input_length=bytes_read-1; // ignore trailing newline
-    input_buffer->buffer[bytes_read - 1] = 0;// strip newline
+    input_buffer->input_length = bytes_read - 1; // ignore trailing newline
+    input_buffer->buffer[bytes_read - 1] = 0;    // strip newline
 }
 
-void close_input_buffer(InputBuffer* input_buffer){
-    free (input_buffer->buffer);
+void close_input_buffer(InputBuffer *input_buffer)
+{
+    free(input_buffer->buffer);
     free(input_buffer);
+}
+
+MetaCommandResult handle_meta_command(InputBuffer *input_buffer)
+{
+    if (strcmp(input_buffer->buffer, ".exit") == 0)
+        exit(EXIT_SUCCESS);
+    else
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+}
+
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
+{
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0)
+    {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if (strcmp(input_buffer->buffer, "select") == 0)
+    {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement *statement)
+{
+    switch (statement->type)
+    {
+    case (STATEMENT_INSERT):
+        printf("This is where we would do an insert.\n");
+        break;
+    case (STATEMENT_SELECT):
+        printf("This is where we would do a select.\n");
+        break;
+    }
 }
 
 int main(int argc, char *arg[])
@@ -53,13 +114,32 @@ int main(int argc, char *arg[])
         print_prompt();
         read_input(input_buffer);
 
-        if (strcmp(input_buffer->buffer, ".exit") == 0)
+        // handle non-SQL statements (begin with `.`)
+        if (input_buffer->buffer[0] == '.')
         {
-            close_input_buffer(input_buffer);
-            printf("Hello");
-            exit(EXIT_SUCCESS);
+            switch (handle_meta_command(input_buffer))
+            {
+            case (META_COMMAND_SUCCESS):
+                continue;
+            case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+                continue;
+            }
         }
-        else
-            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+
+        // SQL command
+        Statement statement;
+        switch (prepare_statement(input_buffer, &statement))
+        {
+        case (PREPARE_SUCCESS):
+            break;
+        case (PREPARE_UNRECOGNIZED_STATEMENT):
+            printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
+            continue;
+        }
+
+        // Prepared SQL statement
+        execute_statement(&statement);
+        printf("Executed.\n");
     }
 }
